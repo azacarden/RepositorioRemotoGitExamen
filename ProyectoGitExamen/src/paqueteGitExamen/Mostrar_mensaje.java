5b081f3bf8a5fc8ee1ecfb8ba25f611b305e556e
Azi <Azi@Pc-de-Aza>
2024-05-16 18:16:09 +0200

    cambios realizados

---
+++ ProyectoGitExamen/src/paqueteGitExamen/Mostrar_mensaje.java
@@ -0,0 +1,10 @@
+package paqueteGitExamen;
+
+public class Mostrar_mensaje {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto es un mensaje para mostrar en el examen");
+	}
+
+}
