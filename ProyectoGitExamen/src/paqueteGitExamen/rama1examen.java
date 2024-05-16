9f5a4c426fad0b21bcab2f0f30a0c528b225d4a9
Azi <Azi@Pc-de-Aza>
2024-05-16 18:32:29 +0200

    rama1examen

---
+++ ProyectoGitExamen/src/paqueteGitExamen/rama1examen.java
@@ -0,0 +1,11 @@
+package paqueteGitExamen;
+
+public class rama1examen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("estoy creando un mensaje");
+		System.out.println("para la rama1examen");
+	}
+
+}
